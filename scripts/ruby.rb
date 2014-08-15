8148a4fbbdb8933f99b2ab134daa114b474c3979
pocke <p.ck.t22@gmail.com>
2014-08-15 20:49:21 +0900

    add ruby classes

--- scripts/ruby.rb
+++ scripts/ruby.rb
@@ -1,12 +1,15 @@
 # Usage
 # bitclust list --method | ruby THIS_FILE.rb > ruby.dict
 
-
-STDIN.map do |line|
+lines = `bitclust list --method`.each_line.map do |line|
   line =~ /^.+[#.]([a-zA-Z0-9_?!]+)$/
   $1
 end.select do |name|
   name and name.size > 2
-end.sort.uniq.each do |name|
+end
+
+lines += `bitclust list --class`.each_line.select{|x| x.size > 2}
+
+lines.sort.uniq.each do |name|
   puts name
 end
